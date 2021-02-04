96a2d4a098965dcd60699805075beab87fb9d264
Emilie <martelemilie@hotmail.fr>
2021-02-04 11:28:24 +0100

    update style home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImgCode from '../../asset/images/code.png';
 import style from '../../asset/css/HomePage.module.css';
 
 function Home() {
@@ -20,19 +21,24 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="container">
+      <div className="container p-0">
         <section className="about row justify-content-md-center">
-          <h1 className="title m-5">A PROPOS DE MOI</h1>
-          <div className="border-title mx_auto pb-5"> </div>
-          <p>
-            Je suis passionnée de developpement, et j&#39;ai décidé de faire de
-            cette passion mon métier, je me suis donc réorientée. C&#39;est en
-            autodidacte que j&#39;ai commencé, puis j&#39;ai suivie une
-            formation. Désireuse d&#39;apprendre, d&#39;acquérir de nouvelles
-            compétences, je réalise des petits projets perso, je suis
-            l&#39;actualité du web et recherche un challenge professionnel.
-            Alors, n&#39;hésitez pas à me contacter
-          </p>
+          <div className="col p-0">
+            <img className="ImgCode" src={ImgCode} alt="" />
+          </div>
+          <div className="col p-0">
+            <h1 className="title m-5">A PROPOS DE MOI</h1>
+            <div className="border-title mx_auto pb-5"> </div>
+            <p>
+              Je suis passionnée de developpement, et j&#39;ai décidé de faire
+              de cette passion mon métier, je me suis donc réorientée. C&#39;est
+              en autodidacte que j&#39;ai commencé, puis j&#39;ai suivie une
+              formation. Désireuse d&#39;apprendre, d&#39;acquérir de nouvelles
+              compétences, je réalise des petits projets perso, je suis
+              l&#39;actualité du web et recherche un challenge professionnel.
+              Alors, n&#39;hésitez pas à me contacter
+            </p>
+          </div>
         </section>
       </div>
     </div>
